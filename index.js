00af6dc10b2b6b0133f9059bb8fa17054f962eb4
Moulham Hallak <mou990@gmail.com>
2021-05-16 23:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(arr, Object) {
+  
+}
+
+let result = record.find()
+
+
+const inventory = [
+  {name: 'apples', quantity: 2},
+  {name: 'bananas', quantity: 0},
+  {name: 'cherries', quantity: 5}
+];
+
+function isCherries(fruit) {
+  return fruit.name === 'cherries';
+}
