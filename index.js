af49d5379429237a7db7f54cbc7fd009ca4233db
Moulham Hallak <mou990@gmail.com>
2021-05-16 23:06:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,7 @@ function superbowlWin(arr, Object) {
 let result = record.find()
 
 
-const inventory = [
-  {name: 'apples', quantity: 2},
-  {name: 'bananas', quantity: 0},
-  {name: 'cherries', quantity: 5}
-];
-function isCherries(fruit) {
-  return fruit.name === 'cherries';
+function superbowlWin(superbowls){
+  let win = superbowls.find(superbowl => superbowl.result === "W")
+  return win ? win.year : undefined;
 }
-inventory.find(isCherries)
